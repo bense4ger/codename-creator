9ee8997010f1edccbf6b08b3e7fe8eb4aa070961
Ben Seager <github@benseager.co.uk>
2018-01-24 21:01:36 +0000

    Initial commit of main

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		Body:       "Hello, world!",
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handleRequest)
+}
